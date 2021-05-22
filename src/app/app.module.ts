8de73cd9fe95de3ed3b52502cf8a8694e9ec7e48
Cesar Velasquez <ces.velasquez@hotmail.com>
2021-05-21 22:09:07 -0600

    + Angular material && flex layout config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,8 +17,13 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
 
+    // Flex Layout
+    FlexLayoutModule,
+
     // Firebase
     AngularFireModule.initializeApp(environment.firebaseConfig),
+
+    BrowserAnimationsModule,
   ],
   providers: [
     AngularFirestoreModule,
